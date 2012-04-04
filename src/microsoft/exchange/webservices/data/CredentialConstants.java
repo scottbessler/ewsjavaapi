32b60aed6fc13eba6038dc3b49433c9318546bcd
Scott Bessler <scottbessler@gmail.com>
2012-04-04 09:58:04 -0700

    init

---
+++ src/microsoft/exchange/webservices/data/CredentialConstants.java
@@ -0,0 +1,22 @@
+package microsoft.exchange.webservices.data;
+
+public interface CredentialConstants {
+	String URL = "https://10.155.21.99/ews/exchange.asmx";
+	String USERNAME = "administrator";
+	String DOMAIN = "corp.exchange.com";
+	String EMAIL_ID = "administrator@corp.exchange.com";
+	String PASSWORD = "test@123";
+	String ATTENDEE_EMAIL_ID = "rajendra@corp.exchange.com";
+	String ATTENDEE_USERNAME = "rajendra"; 
+	String ATTENDEE_PASSWORD = "wipro@1234";
+	String PROXY_CRED_USERNAME = "ProxyUSERNAME";
+	String PROXY_CRED_PASSWORD = "ProxyPASSWORD";
+	String PROXY_CRED_DOMAIN = "ProxyDOMAIN";
+	String PROXY_HOST = "PROXYHOST";
+	int PROXY_PORT = 80;
+	String PATH = "C:\\Users\\rabanda\\Desktop\\sss.txt"; 
+	String COPYTOFILEPATH = "C:\\Users\\rabanda\\Desktop\\FileAttachmentTest.java"; 
+	String SMTPADDRESS_DISTRIBUTION_GROUP = "testdistgroup@corp.exchange.com";
+	String SMTPADDRESS_ROOM = "Test12345@corp.exchange.com";	 
+	int THREAD_SLEEP_MILLSEC = 5000;
+}
